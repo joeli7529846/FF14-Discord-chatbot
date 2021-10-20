64a65d71790c38cbb87cba877fb59e6181bb6223
unknown <ur06121@walsin.corp>
2021-10-20 13:47:11 +0800

    modify macro page

--- cmds/ask.py
+++ cmds/ask.py
@@ -33,14 +33,14 @@ class ask(Cog_Extension):
     @tasks.loop(hours=1)
     async def read_gsheet(self):
         
-        gc = pygsheets.authorize(service_account_file='google_apikey.json')
+        gc = await pygsheets.authorize(service_account_file='google_apikey.json')
 
         survey_url = 'https://docs.google.com/spreadsheets/d/1C62JiqFM-KPMlwTwFCaH1qutYOexXRo-dxPmtBidfJ0/edit#gid=0'
-        sh = gc.open_by_url(survey_url)
+        sh = await gc.open_by_url(survey_url)
 
-        ws = sh.worksheet_by_title('FF14 QA')
+        ws = await sh.worksheet_by_title('FF14 QA')
 
-        df = ws.get_as_df(empty_value='', include_tailing_empty=False)
+        df = await ws.get_as_df(empty_value='', include_tailing_empty=False)
         #df 存成字典格式
         qa_dict = await pd.Series(df.answer.values,index=df.question).to_dict()
         question_list = await df["question"].tolist()
