be138eabe28ab89ce0bb9959bf87ef05990fe553
unknown <joe7529846@gmail.com>
2021-11-22 17:55:29 +0800

    update ?tr

--- cmds/main.py
+++ cmds/main.py
@@ -127,6 +127,8 @@ class main(Cog_Extension):
                 if len(wordsim_list) > 0:
                     embed.description ="你可能要查詢的詞:\n"+"\n".join(wordsim_list)
                     await message.channel.send(embed=embed)
+                    await asyncio.sleep(20)
+                    await message.delete()
                 else:
                     await message.channel.send("無相關資訊")
 
