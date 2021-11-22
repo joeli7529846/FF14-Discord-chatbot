77f60d5d78355e683ca1f4434009b828f06f0369
unknown <ur06121@walsin.corp>
2021-11-22 09:52:27 +0800

    add channel rule

--- cmds/main.py
+++ cmds/main.py
@@ -46,11 +46,11 @@ class main(Cog_Extension):
             if len(message.attachments) > 0: #Checks if there are attachments
                 pic_ext = ['.jpg','.png','.jpeg']
                 for file in message.attachments:
-                    for ext in pic_ext:
-                        print(file.filename)
-                        if not file.filename.endswith(ext):
-                            await asyncio.sleep(2)
-                            await message.delete()
+                    
+                    print(file.filename)
+                    if file.filename[-3:] not in pic_ext:
+                        await asyncio.sleep(2)
+                        await message.delete()
 
         #翻譯
         if message.content.startswith('?tr '):
